758724bc0fca4209afb1e64ddfc1092f39d2141c
Olena <sitnikovahelen@gmail.com>
2018-05-31 18:51:20 +0300

    Menu

--- src/main/java/ua/dp/edu/crypto/controller/AppController.java
+++ src/main/java/ua/dp/edu/crypto/controller/AppController.java
@@ -5,6 +5,7 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
@@ -99,5 +100,9 @@ public class AppController implements Initializable
         Stage stage = new Stage();
         stage.setScene(new Scene(root, 550, 350));
         stage.show();
+    }
+
+    public void close(ActionEvent actionEvent) {
+        System.exit(0);
     }
 }
